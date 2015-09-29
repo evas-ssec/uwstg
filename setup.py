716760b7ffcbbd30703a503d7bebc5a236438c11
Eva Schiffer <evas@ssec.wisc.edu>
2015-09-29 17:33:56 -0500

    cleaning up setup requirements

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ python setup.py install --install-dir=$HOME/Library/Python
 from setuptools import setup, find_packages
 
 setup( name="spacetimegrid", 
-       version="0.1", 
+       version="0.2",
        zip_safe = True,
        entry_points = { 'console_scripts': [ 'stg = stg.space_time_gridding:main', 'stg_plot = stg.plot_tools:main' ] },
        packages = ['stg'], #find_packages('.'),
-       install_requires=[ 'numpy>=1.8.0', 'scipy', 'pyhdf', 'netCDF4', 'matplotlib', 'mpl_toolkits' ],
+       install_requires=[ 'numpy>=1.8.0', 'scipy', 'pyhdf', 'netCDF4', 'matplotlib', ], # 'mpl_toolkits', ],
        #package_data = {'': ['*.txt', '*.gif']}
        )
 
